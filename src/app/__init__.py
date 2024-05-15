0d40388a61eeba7844f5e476f65bc1a96366d2ab
singhdevhub-lovepreet <lpskshorts@gmail.com>
2024-05-15 22:13:20 +0530

    Integrated Kafka

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,16 +1,26 @@
 from flask import Flask
 from flask import request, jsonify
 from service.messageService import MessageService
+from kafka import KafkaProducer
+import json
+
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messageService = MessageService()
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 @app.route('/v1/ds/message', methods=['POST'])
 def handle_message():
     message = request.json.get('message')
     result = messageService.process_message(message)
+    serialized_result = result.json()
+
+    # Send the serialized result to the Kafka topic
+    producer.send('expense_service', serialized_result)
+
     return jsonify(result)
 
 @app.route('/', methods=['GET'])
