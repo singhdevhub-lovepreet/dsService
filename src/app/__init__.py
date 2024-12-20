526a98b0b6f08eca9b5a5375e5a3eab9134652cb
singhdevhub-lovepreet <singhdevhub@gmail.com>
2024-12-20 12:50:18 +0530

    Adding x-user-id

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -20,11 +20,16 @@ producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers,
 
 @app.route('/v1/ds/message', methods=['POST'])
 def handle_message():
+    user_id = request.headers.get('x-user-id')
+    if not user_id:
+        return jsonify({'error': 'x-user-id header is required'}), 400
+
     message = request.json.get('message')
     result = messageService.process_message(message)
 
     if result is not None:
         serialized_result = result.serialize()
+        serialized_result['user_id'] = user_id
         producer.send('expense_service', serialized_result)
         return jsonify(serialized_result)
     else:
@@ -38,6 +43,5 @@ def handle_get():
 def health_check():
     return 'OK'
 
-
 if __name__ == "__main__":
-    app.run(host="localhost", port= 8010 ,debug=True)
+    app.run(host="localhost", port=8010, debug=True)
